bed5654b207b2f969c6c3cc69e84668e92e658a6
Max Fredis Tovar Hernández <maxther911@gmail.com>
2019-09-02 06:28:22 -0500

    Adicion fuentes init

---
+++ src/main/java/net/mrsistemas/healthy/ia/HealthyIA.java
@@ -0,0 +1,13 @@
+package net.mrsistemas.healthy.ia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyIA {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyIA.class, args);
+    }
+
+}
---
+++ src/test/java/net/mrsistemas/healthy/ia/HealthyIATests.java
@@ -0,0 +1,16 @@
+package net.mrsistemas.healthy.ia;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HealthyIATests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
